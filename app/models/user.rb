f1dd2bacea54e3ca7096cea6d6cffeeac8d074bb
maksimgm <maksimmamrikov@gmail.com>
2015-11-02 10:57:43 -0800

    refactor user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ActiveRecord::Base
-  has_many :trips
-  
+  has_many :trips, dependent: :destroy
   has_secure_password
   
-  validates :username, presence: true
-  validates :bio, presence: true
-  validates :profile_pic, presence: true
+  validates :profile_pic, :username, :bio, presence: true
   validates :username, uniqueness: true
 end
