a58a82d9b692036fee4f4b003606ee0e4eb489b7
Donaldo S <DonaldoS@Donaldos-MacBook-Pro-2.local>
2015-11-02 00:50:46 -0800

    add entry model tests

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,4 @@
 class Entry < ActiveRecord::Base
+ 	validates_presence_of :id, :title, :location, :summary, :cost, :image, :video_url, :trip_id
   belongs_to :trip
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Entry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Entry do 
+	it {is_expected.to respond_to :title}
+	it {is_expected.to respond_to :location}
+	it {is_expected.to respond_to :summary}
+	it {is_expected.to respond_to :cost}
+	it {is_expected.to respond_to :image}
+	it {is_expected.to respond_to :video_url}
+
+	it {should belong_to :trip}
+
 end
