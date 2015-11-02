0179742c3bd09e9a9184922b60efbd1ff8dbfd1d
aaron <harms280@gmail.com>
2015-11-01 20:36:26 -0800

    wrote model tests for trip

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Trip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Trip do
+  it {is_expected.to respond_to :title}
+  it {is_expected.to respond_to :location}
+  it {is_expected.to respond_to :duration}
+  it {is_expected.to respond_to :summary}
+  it {is_expected.to respond_to :budget}
+  it {is_expected.to respond_to :like}
+  it {is_expected.to respond_to :video_url}
+
+  it {is_expected.to have_many :entries}
+  it {is_expected.to belong_to :user}
 end
