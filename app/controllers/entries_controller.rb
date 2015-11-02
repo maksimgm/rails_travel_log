ffafb71f6e52a0e703f9ddf978d3e2d9d844c0f0
Donaldo S <DonaldoS@Donaldos-MacBook-Pro-2.local>
2015-11-02 11:51:49 -0800

    code entry controller actions

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,21 +5,40 @@ class EntriesController < ApplicationController
 
   def new
   	@entry = Entry.new
+  end  
+
+  def create
+  	@entry = Entry.new entry_params
+
+  	if @entry.save
+  		flash[:success] = "Entry created"
+  		redirect_to user_trip_entries_path
+  	else
+  		render :new
+  	end
   end
 
   def edit
-  	@entry = Entry.find(params(:id))
+  	@entry = Entry.find(params[:id])
   end
 
   def update
-  	@entry = Entry.find(params(:id))
+  	@entry = Entry.find(params[:id])
+
+  	if @entry.update(entry_params)
   end
 
   def destroy
-  	@entry = Entry.find(params(:id))
+  	@entry = Entry.find(params[:id])
   	@entry.destroy
 
   	# where to redirect_to?
   	# redirect_to user_trip_entries_path
+  end
+
+  private
+
+  def entry_params
+		params.require(:entry).permit(:title, :location, :summary, :cost, :image, :video_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   
-  get 'entries/index'
-
-  get 'entries/new'
-
-  get 'entries/edit'
-
-  root 'users#index'
-
-  resources :users do
+	resources :users do
     resources :trips do
       resources :entries
     end
