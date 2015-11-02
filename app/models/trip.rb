5b5c3d1732f680c30e138d37d76263c3f0237517
aaron <harms280@gmail.com>
2015-11-02 09:48:51 -0800

    update tests to check presence, must update like to likes in test

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,6 @@
 class Trip < ActiveRecord::Base
   belongs_to :user
   has_many :entries
+
+  validates :title, :location, :summary, presence: true
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 describe Trip do
-  it {is_expected.to respond_to :title}
-  it {is_expected.to respond_to :location}
-  it {is_expected.to respond_to :duration}
-  it {is_expected.to respond_to :summary}
-  it {is_expected.to respond_to :budget}
-  it {is_expected.to respond_to :like}
-  it {is_expected.to respond_to :video_url}
+  subject {Trip.new(title: "My trip to Peru", location: "Peru", summary:"Gonna travel around Peru for the next 2 weeks. Follow me.", budget:2000, duration:"1 month")}
+
+  [:title,:location,:duration,:summary,:budget,:like,:video_url].each do |x|
+    it {is_expected.to respond_to x}
+  end
+
+  [:title,:location,:summary].each do |x|
+    it {is_expected.to validate_presence_of x}
+  end
 
   it {is_expected.to have_many :entries}
   it {is_expected.to belong_to :user}
