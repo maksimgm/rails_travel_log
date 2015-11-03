7af7d1203c8ab8e99f46f93b61951692c32fda13
maksimgm <maksimmamrikov@gmail.com>
2015-11-02 16:06:02 -0800

    add login, logout, and signup path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get "/login", to: "users#login", as: "login"
+
+  post "/login", to: "users#attempt_login"
+
+  get "/signup", to: "users#signup", as: "signup"
+
+  post "/signup", to: "users#create"
+
+  delete "/logout", to: "users#logout", as: "logout"
   
 	resources :users do
     resources :trips do
