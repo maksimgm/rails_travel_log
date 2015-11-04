9ef640c803cb823d187595c03df4bb5ea3df9727
Donaldo S <DonaldoS@Donaldos-MacBook-Pro-2.local>
2015-11-03 17:14:28 -0800

    Add google maps api autocomplete

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,12 +1,9 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+
+// Adds autocomplete to entry form location text field
 $(document).ready(function() {
-	var input = $('#entry_location')[0];
-	console.log("Test");
-	console.log(input);
-
+	var input = $('.search_location')[0];
   var autocomplete = new google.maps.places.Autocomplete(input);
-  console.log(autocomplete);
-
 });
