6bb5559364b4ba69dab475e851db6136ea390b37
aaron <harms280@gmail.com>
2015-11-05 14:34:48 -0800

    add search for entries

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def search
     @trips = Trip.search(params[:search])
+    @entries = Entry.search(params[:search])
   end
   
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,12 @@
 class Entry < ActiveRecord::Base
  	validates_presence_of :title, :location, :summary, :cost, :image, :video_url, :trip_id
   belongs_to :trip
+
+  def self.search(search)
+    if search
+      where("lower(location) LIKE ? OR lower(title) LIKE ?", "%#{search.downcase}%", "%#{search.downcase}%")
+    else
+      find(:all)
+    end
+  end
 end
