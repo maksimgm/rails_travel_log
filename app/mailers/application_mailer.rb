ee61c7cb15ef00276a9220a99a8a265c9c6f6dd2
maksimgm <maksimmamrikov@gmail.com>
2015-11-05 17:51:08 -0800

    change url host to heroku link

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "travel-log@travel-log.com"
   # CHANGE OCALHOST TO HEROKU URL!!!!
-  default_url_options[:host] = "localhost:3000"
+  default_url_options[:host] = "https://mysterious-oasis-5854.herokuapp.com/"
 end
